a955402d7f2df06090a96f3285ceb347bdc7920e
Helen King <helenking029@gmail.com>
2019-03-11 21:39:39 -0700

    Update shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,39 +1,36 @@
 shopping_list = []
 
 def show_help():
-  print("What should we pick up from the store?")
-  print("""
-  Enter 'DONE' to stop adding items.
-  Enter 'HELP' for this help.
-  Enter 'SHOW' for your current shopping list.
-  """)
+	print("What should we pick up from the store?")
+	print("""
+	Enter 'DONE' to stop adding items.
+	Enter 'HELP' for this help.
+	Enter 'SHOW' for your current shopping list.
+	""")
 
-  
 def add_to_list(item):
-  shopping_list.append(item)
-  print("Added! List has {} item(s).".format(len(shopping_list)))
-  
-    
-def show_list():
-  print("Here's your list:")
-  for item in shopping_list:
-    print(item)
-  
-show_help()
-while True:
-  new_item = input("> ")
-  
-  if new_item == "DONE":
-    break
-  elif new_item == "HELP":
-    show_help()
-    continue
-  elif new_item == "SHOW":
-    show_list()
-    continue
-    
-  add_to_list(new_item)
+	shopping_list.append(item)
+	print("Added! List has {} item(s).".format(len(shopping_list)))
 
-show_list()
-#print("Shopping List Contains:")
-#print(shopping_list)
+def show_list():
+	print("Here's your list:")
+	for item in shopping_list:
+		print(item)
+
+show_help()
+
+while True:
+	new_item = input("> ")
+	if new_item == "DONE":
+	break
+	elif new_item == "HELP":
+		show_help()
+		continue
+	elif new_item == "SHOW":
+		show_list()
+		continue
+
+	add_to_list(new_item)
+
+print("Shopping List Contains:")
+print(shopping_list)
